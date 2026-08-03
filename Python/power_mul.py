d57a8d788d37c7400297324fe94c8a0775ad2dd3
MADARA143390 <ragu26555@gmail.com>
2026-08-03 23:59:51 +0530

    Files added

---
+++ Python/power_mul.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input("Enter A :"))
+b=int(input("Enter B :"))
+c=int(input("Enter C :"))
+d=int(input("Enter D :"))
+a1=a**b
+b1=c**d
+print(str(a1+b1))
